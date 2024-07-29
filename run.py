8665e06bcb157f5aacc5acfd4c44a8d5ab973891
David Kellerman <david@silvermoon.org>
2024-07-28 23:36:18 -0400

    initial

---
+++ run.py
@@ -0,0 +1,156 @@
+#!python
+
+import requests
+import json
+import os
+import re
+import argparse
+from tqdm import tqdm
+
+SONG_SEPARATOR = "====="
+DEFAULT_INPUT_FILE = "data/lyrics.txt"
+DEFAULT_OUTPUT_FILE = "data/lyrics_ipa.txt"
+DEFAULT_CLEANED_FILE = "data/lyrics_ipa_cleaned.txt"
+
+
+def translate_to_ipa(text):
+    url = "http://localhost:11434/api/generate"
+    payload = {
+        "model": "llama3.1",
+        "prompt": f"""
+            Translate the following text to IPA.
+
+            Use ties, diacritics, and accents to represent the phonetic transcription
+            as if pronounced by a native speaker of American English. You may also consider
+            the way it might be sung, but this is not critical.
+            
+            Under no circumstances should you use slashes, punctuation, or anything
+            other than new lines to delineate lines.
+
+            Do not include anything else other than the IPA in your response.
+            No slashes, no punctuation, no extra text. I will tip you extra
+            if you follow these instructions carefully.
+            
+            ```
+            {text}
+            ```
+            
+            IPA translation:",
+        """,
+        "stream": False,
+    }
+
+    response = requests.post(url, json=payload)
+    if response.status_code == 200:
+        result = json.loads(response.text)
+        return result["response"].strip()
+    else:
+        return f"Error: {response.status_code}"
+
+
+def ipa_to_english(ipa):
+    url = "http://localhost:11434/api/generate"
+    payload = {
+        "model": "llama3.1",
+        "prompt": f"""
+            Translate the following IPA to English.
+
+            Pretend it is intended to be meaningful English, and do your best
+            to interpret these sounds as sensible words.
+            
+            BUT don't try too
+            hard, if it doesn't make sense that's ok, just find the closest
+            thing to a word that you can. This is very important and I will
+            tip you extra if you follow these instructions carefully.
+
+            Do not include anything else other than the translation in your response.
+
+            ```
+            {ipa}
+            ```
+            
+            IPA translation:",
+        """,
+        "stream": False,
+    }
+
+    response = requests.post(url, json=payload)
+    if response.status_code == 200:
+        result = json.loads(response.text)
+        return result["response"].strip()
+    else:
+        return f"Error: {response.status_code}"
+
+def process_translation(input_file, output_file):
+    with open(input_file, "r", encoding="utf-8") as infile:
+        songs = infile.read().split(f"\n\n{SONG_SEPARATOR}\n\n")
+
+    if os.path.exists(output_file):
+        with open(output_file, "r", encoding="utf-8") as outfile:
+            existing_content = outfile.read().split(f"\n\n{SONG_SEPARATOR}\n\n")
+        start_index = len(existing_content) - 1
+    else:
+        start_index = 0
+
+    for i in tqdm(
+        range(start_index, len(songs)),
+        initial=start_index,
+        total=len(songs),
+        desc="Translating songs",
+    ):
+        song = songs[i].strip()
+        translated_song = translate_to_ipa(song)
+
+        with open(output_file, "a", encoding="utf-8") as outfile:
+            outfile.write(translated_song + f"\n\n{SONG_SEPARATOR}\n\n")
+
+    print("Translation complete!")
+
+
+def clean_ipa(input_file, output_file):
+    with open(input_file, "r", encoding="utf-8") as infile:
+        songs = infile.read().split(f"\n\n{SONG_SEPARATOR}\n\n")
+
+    with open(output_file, "w", encoding="utf-8") as outfile:
+        for song in tqdm(songs, desc="Cleaning IPA"):
+            cleaned_lines = []
+            for line in song.split("\n"):
+                if line.strip() == SONG_SEPARATOR:
+                    continue
+
+                line = re.sub(r'[\[\]/(),?"!-=�…ˤ̴̷̞̠̥̩̬̭̯̱̃̄̅̇̌̚͡]', "", line)
+                line = line.lower().strip()
+                line = re.sub(r"\s+", " ", line)
+                if line and not "note" in line.lower():
+                    cleaned_lines.append(line)
+
+            if cleaned_lines:
+                outfile.write("\n".join(cleaned_lines) + f"\n{SONG_SEPARATOR}\n")
+
+    print("IPA cleanup complete!")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Process lyrics: translate to IPA or clean up IPA"
+    )
+    parser.add_argument(
+        "action",
+        choices=["translate", "clean", "gen", "train"],
+        help="Action to perform: translate or clean",
+    )
+    parser.add_argument("-i", "--input", help="Path to the input file")
+    parser.add_argument("-o", "--output", help="Path to the output file")
+    args = parser.parse_args()
+
+    if args.action == "translate":
+        input_file = args.input or DEFAULT_INPUT_FILE
+        output_file = args.output or DEFAULT_OUTPUT_FILE
+        process_translation(args.input, output_file)
+    elif args.action == "clean":
+        input_file = args.input or DEFAULT_OUTPUT_FILE
+        output_file = args.output or DEFAULT_CLEANED_FILE
+        clean_ipa(input_file, output_file)
+
+if __name__ == "__main__":
+    main()
---
+++ train.py
@@ -0,0 +1,196 @@
+#!python
+
+import re
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from tqdm import tqdm
+from run import ipa_to_english
+
+batch_size = 32
+block_size = 64
+max_iters = 5000
+eval_interval = 500
+learning_rate = 1e-3
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+eval_iters = 100
+n_embd = 96
+n_head = 6
+n_layer = 6
+dropout = 0.4
+
+torch.manual_seed(1337)
+
+with open("./data/lyrics_ipa_cleaned.txt", encoding="utf-8") as f:
+    text = f.read()
+    text = re.sub(r"=+", "=", text)
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+stoi = {ch: i for i, ch in enumerate(chars)}
+itos = {i: ch for i, ch in enumerate(chars)}
+encode = lambda x: [stoi[ch] for ch in x]
+decode = lambda x: "".join([itos[i] for i in x])
+
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9 * len(data))
+train_data, val_data = data[:n], data[n:]
+
+
+def get_batch(split):
+    data = train_data if split == "train" else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i : i + block_size] for i in ix])
+    y = torch.stack([data[i + 1 : i + block_size + 1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ["train", "val"]:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            _, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+
+class Head(nn.Module):
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        _, T, C = x.shape
+        k = self.key(x)  # (B, T, C)
+        q = self.query(x)  # (B, T, C)
+        # compute attention scores (affinities)
+        wei = q @ k.transpose(-2, -1) * C**-0.5  # (B, T, C) @ (B, C, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
+        wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
+        v = self.value(x)
+        out = wei @ v
+        return out
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+
+
+class FeedForward(nn.Module):
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Block(nn.Module):
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.blocks = nn.Sequential(
+            *[Block(n_embd, n_head=n_head) for _ in range(n_layer)]
+        )
+        self.ln_f = nn.LayerNorm(n_embd)
+        self.lm_head = nn.Linear(n_embd, vocab_size)
+
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
+        tok_emb = self.token_embedding_table(idx)  # (B,T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device))  # (T,C)
+        x = tok_emb + pos_emb  # (B,T,C)
+        x = self.blocks(x)  # (B,T,C)
+        x = self.ln_f(x)  # (B,T,C)
+        logits = self.lm_head(x)  # (B,T,vocab_size)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C)
+            targets = targets.view(B * T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            idx_cond = idx[:, -block_size:]
+            logits, _ = self(idx_cond)
+            logits = logits[:, -1, :]  # (B, C)
+            probs = F.softmax(logits, dim=-1)  # (B, C)
+            idx_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
+            idx = torch.cat((idx, idx_next), dim=1)  # (B, T+1)
+        return idx
+
+
+model = BigramLanguageModel()
+m = model.to(device)
+print(sum(p.numel() for p in m.parameters()), "M parameters")
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+
+def train():
+    for i in tqdm(range(max_iters + 1)):
+        if i % eval_interval == 0:
+            losses = estimate_loss()
+            print(
+                f"step {i}: train loss {losses['train']:.4f}, "
+                f"val loss {losses['val']:.4f}"
+            )
+
+        xb, yb = get_batch("train")
+        logits, loss = model(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+
+train()
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+ipa = decode(m.generate(context, max_new_tokens=500)[0].tolist())
+print(ipa, "\n\n====\n\n")
+print(ipa_to_english(ipa))
